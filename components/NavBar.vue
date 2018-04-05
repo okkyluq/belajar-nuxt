e22857f7c8d5a98b4a8a2d76a9c78d3ac30d307d
Luqman <mr.luqman14@gmail.com>
2018-04-05 15:58:55 +0800

    active burger menu

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
-	<nav class="navbar is-transparent">
+	<nav class="navbar is-warning">
 		<div class="navbar-brand">
 			<nuxt-link class="navbar-item" to="/">
 				<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
 			</nuxt-link>
-			<div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+			<div class="navbar-burger burger" data-target="navbarExampleTransparentExample" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
 				<span></span>
 				<span></span>
 				<span></span>
@@ -15,18 +15,6 @@
 			<div class="navbar-start">
 				<nuxt-link to="/" class="navbar-item">Home</nuxt-link>
 				<nuxt-link to="/blog" class="navbar-item">Blog</nuxt-link>
-				<div class="navbar-item has-dropdown is-hoverable">
-					<a class="navbar-link" href="/documentation/overview/start/">
-					Docs
-					</a>
-					<div class="navbar-dropdown is-boxed">
-						<a class="navbar-item" href="/documentation/overview/start/"> Overview </a>
-						<a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/"> Modifiers </a>
-						<a class="navbar-item" href="https://bulma.io/documentation/columns/basics/"> Columns </a>
-						<a class="navbar-item" href="https://bulma.io/documentation/layout/container/"> Layout </a>
-						<a class="navbar-item" href="https://bulma.io/documentation/form/general/"> Form </a>
-					</div>
-				</div>
 			</div>
 		</div>
 	</nav>
